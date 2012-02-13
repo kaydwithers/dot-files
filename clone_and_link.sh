3d0ac1ec346a002b3122fb2e24ec17b402522f8f
Nathan S <nathan@inspire9.com>
2012-02-13 12:49:36 +1100

    update for me

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd &&
-[ -d '.dot-files' ] || git clone git://github.com/nathanscott/dot-files.git .dot-files &&
+[ -d '.dot-files' ] || git clone git://github.com/markbrown4/dot-files.git .dot-files &&
 ls -1d .dot-files/files/.* | while read f; do
   [ "$f" == '.dot-files/.' ] ||
   [ "$f" == '.dot-files/..' ] ||
@@ -9,12 +9,12 @@ ls -1d .dot-files/files/.* | while read f; do
   ln -vsf "$f" .
 done
 
-# cd &&
-# [ -d '.oh-my-zsh' ] || git clone git://github.com/nathanscott/oh-my-zsh.git .oh-my-zsh
-# 
-# cd &&
-# [ -d '.oh-my-zsh' ] || git clone git://github.com/sstephenson/rbenv.git .rbenv
-# 
-# rbenv install || mkdir -p .sources && cd .sources &&
-# git clone git://github.com/sstephenson/ruby-build.git &&
-# cd ruby-build && ./install.sh && rbenv install 1.9.3-p0
+cd &&
+[ -d '.oh-my-zsh' ] || git clone git://github.com/nathanscott/oh-my-zsh.git .oh-my-zsh
+
+cd &&
+[ -d '.oh-my-zsh' ] || git clone git://github.com/sstephenson/rbenv.git .rbenv
+
+rbenv install || mkdir -p .sources && cd .sources &&
+git clone git://github.com/sstephenson/ruby-build.git &&
+cd ruby-build && ./install.sh && rbenv install 1.9.3-p0
